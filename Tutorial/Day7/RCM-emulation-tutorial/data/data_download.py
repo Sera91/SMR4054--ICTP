5f3f8113818ff055b70ad0e5beed86c6f8ba2332
Serafina <eirene1991@gmail.com>
2025-09-16 15:18:43 +0100

    new file:   data/data_download.py 	new file:   data/run_download.sh

---
+++ Tutorial/Day7/RCM-emulation-tutorial/data/data_download.py
@@ -0,0 +1,36 @@
+import requests
+import zipfile
+import sys
+
+Toubkal_username=str(sys.argv[1])
+
+domain = 'NZ'
+DATA_PATH = '/home/'+ Toubkal_username+'/lustre/training_t-7bpcjwtiufg/'+ Toubkal_username+'/SMR4054--ICTP/Tutorial/Day7/RCM-emulation-tutorial/data/CORDEXBench/{domain}'
+os.makedirs(DATA_PATH, exist_ok=True)
+
+
+def download_and_extract(domain, DATA_PATH=DATA_PATH):
+
+    BASE_URL = "https://zenodo.org/records/15797226/files"
+
+    zip_path = os.path.join(DATA_PATH, f"{domain}_domain.zip")
+    download_url = f"{BASE_URL}/{domain}_domain.zip?download=1"
+
+    # Download the zip file
+    with requests.get(download_url, stream=True) as r:
+        r.raise_for_status()
+        with open(zip_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+    # Extract zip contents into DATA_PATH
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(DATA_PATH)
+
+    # Remove the zip file after extraction
+    os.remove(zip_path)
+
+
+
+download_and_extract(domain)
+
---
+++ Tutorial/Day7/RCM-emulation-tutorial/data/run_download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+module purge
+module load cuDNN virtualenv
+module load Autotools/20220317-GCCcore-11.3.0
+module load netCDF-Fortran/4.6.0-iimpi-2022a
+
+#sourcing virtual environment
+source /home/$USER/lustre/training_t-7bpcjwtiufg/shared/Tutenv/bin/activate
+
+#entering the local repo for the tutorial
+
+cd /home/$USER/lustre/training_t-7bpcjwtiufg/$USER/SMR4054--ICTP/Tutorial/Day7/RCM-emulation-tutorial/data/
+
+python data_download.py $USER
+                                    
